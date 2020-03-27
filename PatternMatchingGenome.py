ef850e9ab306943084372805b65c259345da226e
iulia <iuliamarialungu@yahoo.com>
2020-03-27 10:56:38 +0100

    Create Readme

--- PatternMatchingGenome.py
+++ PatternMatchingGenome.py
@@ -12,7 +12,8 @@ def PatternMatching(Pattern, Genome):
             positions.append(index)
     return positions
 
-pattern = "CTTGATCAT"
+pattern1 = "CTTGATCAT"
+pattern2 = "ATGATCAAG" # reverse complement of pattern1
 genomeFile = open(r"vibrio_cholerae_genome.txt")
 genome = genomeFile.read()
 genomeFile.close()
@@ -20,12 +21,27 @@ genomeFile.close()
 
 print("The positions where I found the pattern CTTGATCAT in V.Cholerae genome are: " +
 ', '.join(
-    map(str, PatternMatching(pattern, genome)))
+    map(str, PatternMatching(pattern1, genome)))
+    )
+
+print("The positions where I found the pattern ATGATCAAG in V.Cholerae genome are: " +
+', '.join(
+    map(str, PatternMatching(pattern2, genome)))
     )
 """
 The positions where I found the pattern CTTGATCAT in V.Cholerae genome are:
 60039, 98409, 129189, 152283, 152354, 152411, 163207, 197028, 200160, 357976, 376771, 392723, 532935, 600085, 622755, 1065555
 
-152283, 152354, 152411 form a "clump" a.k.a appear close to each other in a small region of the genome.
+The positions where I found the pattern ATGATCAAG in V.Cholerae genome are: 
+116556, 149355, 151913, 152013, 152394, 186189, 194276, 200076, 224527, 307692, 479770, 610980, 653338, 679985, 768828, 878903, 985368
 
+Conclusion:
+152283, 152354, 152411 for CTTGATCAT form a "clump" a.k.a appear close to each other in a small region of the genome.
+idem for 151913, 152013, 152394 for ATGATCAAG
+
+CTTGATCAT
+||||||||
+GAACTAGTA
+
+An Ori usually has ~500 nucleotides. For v.cholerae the number is 540
 """
---
+++ README.md
@@ -0,0 +1,5 @@
+## Biology meets Programming
+
+Free [course](https://www.coursera.org/learn/bioinformatics/) on Coursera platform.
+
+This is a repository to organize personal code and comments as I am going through the [Bioinformatics Specialization](https://www.coursera.org/specializations/bioinformatics).
