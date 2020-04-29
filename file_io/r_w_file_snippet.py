c11d0d43c541640844af08709c1fa28bccd6e59c
iulia <iuliamarialungu@yahoo.com>
2020-04-29 13:49:02 +0200

    prepare profile from input

--- file_io/r_w_file_snippet.py
+++ file_io/r_w_file_snippet.py
@@ -1,19 +1,14 @@
-
-
 if __name__ == "__main__":
     import subprocess
-    from outputter import outputter
-    from inputter import inputter
+    from file_io import outputter, inputter
     with open('../../Downloads/dataset_9_3.txt') as input_file:
-        args = [inputter(word) for line in input_file for word in line.split()]
+        args = [inputter.inputter(word) for line in input_file for word in line.split()]
 
     # produce output here
     output = "_______Missing output!______"
 
     with open('output.txt', "w") as output_file:
-        output_file.write(outputter(output))
-
-    # display in default GUI
-    subprocess.run(['open', 'output.txt'])
+        output_file.write(outputter.outputter(output))
+        subprocess.run(['open', 'output.txt']) # display in default GUI
 
 
